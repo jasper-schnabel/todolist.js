012cdfcbafb04df0436041c122090755cc10552e
moritz.wachter <moritz.wachter@endava.com>
2022-01-10 18:39:55 +0100

    remove node-sass-middleware

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var scssMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var todoRouter = require('./routes/todo');
@@ -18,12 +17,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(scssMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
-}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
